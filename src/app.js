c4d90413b2035ea731525567d93cb9d85276e818
Roberto Jacobo <robjacobox@gmail.com>
2023-03-10 11:39:56 -0600

    last changes handlebars added

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+var path = require('path');
 const { Server } = require("socket.io");
 const handlebars = require("express-handlebars");
 const viewRoutes = require("./routes/index.routes");
@@ -6,12 +7,12 @@ const viewRoutes = require("./routes/index.routes");
 const PORT = 8080;
 const app = express();
 
-// Template engine
-app.engine("handlebars", handlebars.engine());
-
-app.set("views", __dirname + "/views");
+// Template engine (Handlebars)
+app.engine(".handlebars", handlebars.engine());
 
 app.set("view engine", "handlebars");
+
+app.set('views', path.join(__dirname, 'views'));
 
 // Middlewares
 app.use(express.json());
--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -5,7 +5,11 @@ const productsRoutes = require("./products/products.routes");
 const router = Router();
 
 router.get('/', (req, res) => {
-  res.render('index', {});
+  res.render('index');
+});
+
+router.get('/about', (req, res) => {
+  res.render('about');
 });
 
 router.use('/carts', cartsRoutes);
